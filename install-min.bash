f74c8339be3f044f9a2185965ee4473ff2419d8b
AJ ONeal <awesome@coolaj86.com>
2015-03-26 23:42:27 -0600

    also add certs

--- install-min.bash
+++ install-min.bash
@@ -2,10 +2,12 @@
 
 #curl -fsSL https://bit.ly/install-facecards-dev | bash
 
-git clone https://github.com/LDSorg/backend-oauth2-node-passport-example.git facecards.org-nobackend
-pushd facecards.org-nobackend
+git clone https://github.com/LDSorg/backend-oauth2-node-passport-example.git ./facecards.org-nobackend
+pushd ./facecards.org-nobackend
 
-git clone https://github.com/LDSorg/facecards.org-frontend.git frontend
-ln -s frontend/app public
-pushd frontend
+git clone https://github.com/LDSorg/local.ldsconnect.org-certificates.git ./certs
+
+git clone https://github.com/LDSorg/facecards.org-frontend.git ./frontend
+ln -s ./frontend/app public
+pushd ./frontend
 bower install
